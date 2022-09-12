9b8912661736f9cf65750bb72ea16bd63c01a3c7
andarguello <aarguello@martinexsa.com>
2022-09-12 01:24:21 -0600

    ambos ejemplos en R

--- R/svm_lineal.R
+++ R/svm_lineal.R
@@ -1,5 +1,36 @@
 library(e1071)
 
+# Datos, creemos dos grupos claramente separables
+set.seed(34)
+x1 <- runif(30)
+x2 <- 5 * x1 ^ 2 + rnorm(length(x1), sd = 2)
+x <- matrix(c(x1, x2), nrow = 30, ncol= 2)
+y = ifelse(x1 < 0.6, -1, 1)
+plot(x, col=y+3, pch = 19)
+
+dat = data.frame(x, y = as.factor(y))
+
+svmfit = svm(
+  y ~ .,
+  data = dat,
+  kernel = "linear", #como podemos trazar una linea, usamos un kernel lineal
+  cost = 10, # cost = c
+  scale = FALSE)
+print(svmfit) # indica num. de support vectors utilizados, ademas del kernel y tipo
+
+# codigo para graficar el modelo de SVM, podemos volverlo una función
+# Multiplicamos los coeficientes por cada support vector
+beta = drop(t(svmfit$coefs)%*%svmfit$SV)
+beta0 = svmfit$rho
+
+plot(x, col = y + 3, pch = 19)
+points(x[svmfit$index,], pch = 5, cex = 2)
+abline(beta0 / beta[2], -beta[1] / beta[2])
+abline((beta0 - 1) / beta[2], -beta[1] / beta[2], lty = 2)
+abline((beta0 + 1) / beta[2], -beta[1] / beta[2], lty = 2)
+
+
+# Otro ejemplo lineal, pero con datos sobrelapados
 set.seed(101011)
 # Creamos unos datos en dos dimensiones aleatoriamente, puede que tengamos algunos datos mal calsificados por que son aleatorios
 x = matrix(rnorm(60), 30, 2)
@@ -21,19 +52,11 @@ svmfit = svm(
 print(svmfit) # indica num. de support vectors utilizados, ademas del kernel y tipo
 
 # codigo para graficar el modelo de SVM
-grange = apply(x, 2, range)
-n = 75
-x1 = seq(from = grange[1,1], to = grange[2,1], length = n)
-x2 = seq(from = grange[1,2], to = grange[2,2], length = n)
-xgrid = expand.grid(X1 = x1, X2 = x2)
-
-ygrid = predict(svmfit, xgrid)
 # Multiplicamos los coeficientes por cada support vector
 beta = drop(t(svmfit$coefs)%*%svmfit$SV)
 beta0 = svmfit$rho
 
-plot(xgrid, col = c("red", "blue")[as.numeric(ygrid)], pch = 20, cex = .2)
-points(x, col = y + 3, pch = 19)
+plot(x, col = y + 3, pch = 19)
 points(x[svmfit$index,], pch = 5, cex = 2)
 abline(beta0 / beta[2], -beta[1] / beta[2])
 abline((beta0 - 1) / beta[2], -beta[1] / beta[2], lty = 2)
@@ -43,5 +66,5 @@ abline((beta0 + 1) / beta[2], -beta[1] / beta[2], lty = 2)
 set.seed(10)
 test = matrix(rnorm(20, mean=1), 5, 2)
 test_y = predict(svmfit, test)
-points(test[test_y == -1,], col = "black", bg="red", pch=24)
-points(test[test_y == 1,], col = "black", bg="blue",pch=24)
+points(test[test_y == -1,], col ="black", bg=-1 + 3, pch=24)
+points(test[test_y == 1,], col ="black", bg= 1 + 3, pch=24)
--- R/svm_no_lineal.R
+++ R/svm_no_lineal.R
@@ -1,9 +1,110 @@
+# Polinomial
+# Código para generar el area o "grid" de cada clase
+grids = function(x, n = 75) {
+  grange = apply(x, 2, range)
+  x1 = seq(from = grange[1,1], to = grange[2,1], length = n)
+  x2 = seq(from = grange[1,2], to = grange[2,2], length = n)
+  expand.grid(X1 = x1, X2 = x2)
+}
+
+# Ahora, algo más interesante
+set.seed(123)
+theta1 = runif(100, 0,2*pi)
+x1 = matrix(c(cos(theta1) + rnorm(100, 0, 0.03),
+            sin(theta1) + rnorm(100, 0, 0.03)),
+            nrow = 100, ncol = 2)
+set.seed(321)
+x2 = matrix(runif(200, -0.4, 0.4), 100, 2)
+x=rbind(x1,x2)
+y = c(rep(1, dim(x1)[1]),
+      rep(-1, dim(x2)[1]))
+plot(x, col = y + 3, pch=20)
+dat = data.frame(x, y=as.factor(y))
+
+svmfit = svm(
+  y ~ .,
+  data = dat,
+  kernel = "polynomial", #como no podemos trazar una linea, usamos un kernel polinomial
+  degree = 2, # probemos con uno de grado 2 primero
+  cost = 10, # cost = c
+  scale = FALSE)
+print(svmfit) # indica num. de support vectors utilizados, ademas del kernel y tipo
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = c("red","blue")[as.numeric(ygrid)], pch = 20, cex = .2)
+points(x, col = y + 3, pch = 19)
+points(x[svmfit$index,], pch = 5, cex = 2)
+
+# Gaussiano
+svmfit = svm(
+  y ~ .,
+  data = dat,
+  kernel = "radial", #como no podemos trazar una linea, usamos un kernel polinomial
+  cost = 10, # cost = c
+  scale = FALSE)
+print(svmfit) # indica num. de support vectors utilizados, ademas del kernel y tipo
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = c("red","blue")[as.numeric(ygrid)], pch = 20, cex = .2)
+points(x, col = y + 3, pch = 19)
+points(x[svmfit$index,], pch = 5, cex = 2)
+
+# Otro ejemplo gaussiano
 # Nuestro working directory será donde esté el archivo, para tener acceso al .rda
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 load(file = "ESL.mixture.rda")
 names(ESL.mixture)
 rm(x,y)
 attach(ESL.mixture)
-
+# Veamos los datos
 plot(x, col= y + 1)
+
 # Notemos que hay mucho más overlap de datos
+dat = data.frame(x, y = factor(y))
+
+# Probemos primero con un polinomio de grado 5
+svmfit = svm(y ~ .,
+             data = dat,
+             scale = FALSE,
+             kernel = "polynomial",
+             degree = 5,
+             cost = 10)
+print(svmfit)
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = as.numeric(ygrid), pch = 20, cex = .2)
+points(x, col = y + 1, pch = 19)
+
+svmfit = svm(y ~ .,
+          data = dat,
+          scale = FALSE,
+          kernel = "radial",
+          cost = 10)
+print(svmfit)
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = as.numeric(ygrid), pch = 20, cex = .2)
+points(x, col = y + 1, pch = 19)
+
+# Veamos qué pasa si aumentamos y disminuimos el cost
+svmfit = svm(y ~ .,
+             data = dat,
+             scale = FALSE,
+             kernel = "radial",
+             cost = 20)
+print(svmfit)
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = as.numeric(ygrid), pch = 20, cex = .2)
+points(x, col = y + 1, pch = 19)
+
+svmfit = svm(y ~ .,
+             data = dat,
+             scale = FALSE,
+             kernel = "radial",
+             cost = 5)
+print(svmfit)
+xgrid = grids(x)
+ygrid = predict(svmfit, xgrid)
+plot(xgrid, col = as.numeric(ygrid), pch = 20, cex = .2)
+points(x, col = y + 1, pch = 19)
